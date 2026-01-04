93e54e4365f7aa473913a4991df2c400e5bd012e
_nebula <41904486+misternebula@users.noreply.github.com>
2026-01-04 02:18:01 +0000

    don't serialize STRFile

--- Assets/Scripts/AttributeHandler.cs
+++ Assets/Scripts/AttributeHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Assets.Scripts
@@ -11,6 +12,7 @@ namespace Assets.Scripts
 		/// <summary>
 		/// The filepath of the .str file this simgroup is contained in.
 		/// </summary>
+		[NonSerialized]
 	    public string STRFile;
 
 	    public abstract void HandleAttributes(BinaryReader reader, SimGroup.AttrPacket attrPacket);
